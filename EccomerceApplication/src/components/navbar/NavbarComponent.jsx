4027ca19b1c5864c5347c26033d09ef22734c9e7
Travis <travisjb0@gmail.com>
2023-11-15 15:47:36 -0800

    Added sitelogo and added to navbar

--- EccomerceApplication/src/components/navbar/NavbarComponent.jsx
+++ EccomerceApplication/src/components/navbar/NavbarComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-
+import wiccanLogo from './navbarResources/wiccanCollectiveLogo.png';
 
 const NavbarComponent = () => {
 
@@ -9,15 +9,13 @@ const NavbarComponent = () => {
         <div className='gray-950'>
              <Navbar className="bg-body-tertiary">
       <Container className='flex justify-center text-yellow-200 font-bold font-Harrington'>
-        <Navbar.Brand href="#home">Wiccan Collection</Navbar.Brand>
+        <Navbar.Brand href="#home"><img className='w-[25%]' src={wiccanLogo} /></Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-        <Navbar.Text>
-           Home <a href="#login">Mark Otto</a>
+        <Navbar.Text className='ml-[100%]'>
+            <a href="#login">Mark Otto</a>
           </Navbar.Text>
-          <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
-          </Navbar.Text>
+          
           <div style={{ border: '1px solid white', width: '100%', margin: '10px 0' }} />
         </Navbar.Collapse>
       </Container>
