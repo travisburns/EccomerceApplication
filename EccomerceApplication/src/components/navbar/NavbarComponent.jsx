e7b74289d2ecaff5915049b81ea85907d1194bf0
Travis <travisjb0@gmail.com>
2023-11-15 16:28:55 -0800

    Added all design for now for navbar on all device sizes

--- EccomerceApplication/src/components/navbar/NavbarComponent.jsx
+++ EccomerceApplication/src/components/navbar/NavbarComponent.jsx
@@ -1,40 +1,55 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import React from
+ 
+'react';
+import Container from
+ 
+'react-bootstrap/Container';
+import Navbar from
+ 
+'react-bootstrap/Navbar';
 import wiccanLogo from './navbarResources/wiccanCollectiveLogo.png';
+import NavbarText from 'react-bootstrap/esm/NavbarText';
+import { useMediaQuery } from 'react-responsive';
 
 const NavbarComponent = () => {
+  const isMdOrGreater = useMediaQuery({ query: '(min-width: 768px)' });
 
   return (
-        <div className='gray-950'>
-             <Navbar className="bg-body-tertiary">
-      <Container className='flex justify-center text-yellow-200 font-bold font-Harrington'>
-        <Navbar.Brand href="#home"><img className='w-[25%]' src={wiccanLogo} /></Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-        <Navbar.Text className='ml-[100%]'>
-            <a href="#login">Mark Otto</a>
-          </Navbar.Text>
-          
-          <div style={{ border: '1px solid white', width: '100%', margin: '10px 0' }} />
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-            
-            
-            
-            {/* <ul className='flex justify-center text-yellow-200 font-bold font-Harrington'>
-                <li className='mr-[.5rem]'>HOME</li>
-                <li  className='mr-[.5rem]'>ABOUT</li>
-                <li  className='mr-[.5rem]'>PRODUCTS</li>
-                <li  className='mr-[.5rem]'>INFO</li>
-                <li  className='mr-[.5rem]'>PROFILE</li>
-            </ul>
-            <div style={{ border: '1px solid white', width: '100%', margin: '10px 0' }} /> */}
-        </div>
-      
-    
-  )
-}
+    <div className='bg-gray-950'>
+      <Navbar className="bg-body-tertiary">
+        <Container className=' text-yellow-200 font-bold font-Harrington'>
+          <div className='flex'>
+          <Navbar.Brand href="#home" className='md:w-[30%]'>
+            <img className='w-[25%] sm:w-[35%] md:w-[40%] lg:w-[35%]' src={wiccanLogo} />
+          </Navbar.Brand>
+          <Navbar.Toggle />
 
-export default NavbarComponent
+          {isMdOrGreater && (
+            <div className='flex flex-row gap-3 md:text-[1.5rem] md:relative md:top-[1.5rem] lg:text-[1.8rem] lg:top-[2rem] xl:text-[2.1rem] xl:top-[2.4rem] 2xl:text-[2.4rem] 2xl:top-[2.8rem] 3xl:top-[3.2rem] 3xl:text-[3.1rem]'>
+              <NavbarText className='text-yellow-200 font-bold font-Harrington'>
+                <a href='' className=''>About</a>
+              </NavbarText>
+              <NavbarText>
+                <a href=''>Our Shop</a>
+              </NavbarText>
+              <NavbarText>
+                <a href=''>Reviews</a>
+              </NavbarText>
+              <Navbar.Text className=''>
+                <a href="#login">Mark Otto</a>
+              </Navbar.Text>
+            </div>
+          )}
+          </div>
+          
+
+          <Navbar.Collapse className="justify-content-end">
+            {/* Other Navbar.Collapse content */}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavbarComponent;
