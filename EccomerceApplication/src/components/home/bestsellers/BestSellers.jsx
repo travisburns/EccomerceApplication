f9a84705433ec06886ce617b8888771653b18089
Travis <travisjb0@gmail.com>
2023-11-16 17:46:14 -0800

    Added a substantially better design for the product cards that matches the magic feel/theme of the store

--- EccomerceApplication/src/components/home/bestsellers/BestSellers.jsx
+++ EccomerceApplication/src/components/home/bestsellers/BestSellers.jsx
@@ -9,17 +9,6 @@ const BestSellers = () => {
     <div className='pt-[50px] mx-6 lg:mx-12 xl:mx-24'>
     <h1 className='text-3xl font-bold text-yellow-100 text-shadow-mdtext-white text-[2rem] font-Harrington text-center relative z-[0] 3xl:text-[5rem]'>Best Sellers</h1>
 
-<Card className='bg-black bg-opacity-80 text-white w-[50%] border border-[0.1] border-amber-100' style={{ backgroundColor: 'rgba(0, 0, 0, 0.4)' }}> 
-<Card.Img variant="top" src={crystalGem} />
-<Card.Body>
-  <Card.Title className='text-1xl bg-slate-900 border border-[0.1] border-amber-100 font-bold  text-yellow-100 text-shadow-mdtext-white text-[1.7rem] font-Harrington text-center'>Ruby Circluit</Card.Title>
-  <Card.Text className='  text-yellow-100 text-shadow-mdtext-white text-[1rem] font-Harrington text-center'>
-    Some quick example text to build on the card title and make up the
-    bulk of the card's content.
-  </Card.Text>
-  <Button variant="primary" className='bg-slate-900 text-yellow-100 font-extrabold text-shadow-mdtext-white text-[1rem] font-Harrington text-center border border-[0.1] border-amber-100'>View</Button>
-</Card.Body>
-</Card>
 
     </div>
    
--- EccomerceApplication/src/components/products/Product.jsx
+++ EccomerceApplication/src/components/products/Product.jsx
@@ -13,15 +13,18 @@ const Product = () => {
         
 
         <div className=''>
-            <div key={product.id} class="mx-auto mt-11 w-80 transform overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
+            <div key={product.id} class="mx-auto mt-11 w-80 transform overflow-hidden  bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
   <img class="h-48 w-full object-cover object-center" src={product.image} alt="Product Image" />
   <div class="p-4 font-Harrington">
-    <h2 class="mb-2 text-lg font-medium dark:text-white text-gray-900 ">{product.title}</h2>
-    <p class="mb-2 text-base dark:text-gray-300 text-gray-700">{product.description}</p>
-    <div class="flex items-center">
-      <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">{product.price}</p>
-      <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300">$25.00</p>
-      <p class="ml-auto text-base font-medium text-green-500">20% off</p>
+    <div className='border border-[0.1] border-amber-100'>
+    <h2 class=" text-[1.5rem] font-medium dark:text-white text-gray-900 bg-slate-900 border border-[0.1] border-amber-100">{product.title}</h2>
+    <p class=" text-base dark:text-gray-300 text-gray-700 bg-slate-900">{product.description}</p>
+    </div>
+    
+    <div class="flex items-center mt-2">
+      <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white border border-[0.1] border-amber-100 bg-slate-900">${product.price}</p>
+      <p class="text-base  font-medium text-gray-500 line-through dark:text-gray-300 border border-[0.1] border-amber-100 bg-slate-900">$25.00</p>
+      <p class="ml-auto text-base font-medium text-green-500 border border-[0.1] border-amber-100 bg-slate-900">20% off</p>
     </div>
   </div>
 </div>
@@ -30,7 +33,6 @@ const Product = () => {
 
   return (
     <div>
-      <h1>Our Products</h1>
       {cards}
     </div>
   )
