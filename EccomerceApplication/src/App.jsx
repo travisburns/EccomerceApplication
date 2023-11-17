55fd503f91a2264452ed57f97ac54a3aa8e30d38
Travis <travisjb0@gmail.com>
2023-11-16 17:23:49 -0800

    Added React-router v6

--- EccomerceApplication/src/App.jsx
+++ EccomerceApplication/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Home from './components/home/Home'
-
 import NavbarComponent from './components/navbar/NavbarComponent'
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
