9945f1c6fb0b1d749aec3c50e60388bf9e7d8b74
Travis <travisjb0@gmail.com>
2023-11-28 16:47:28 -0800

    added way better navbar design

--- EccomerceApplication/src/components/navbar/NavbarComponent.jsx
+++ EccomerceApplication/src/components/navbar/NavbarComponent.jsx
@@ -18,10 +18,10 @@ const NavbarComponent = ({fixed}) => {
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <NavLink to="/">
             <a
-              className="text-sm font-bold leading-relaxed inline-block mr-4  whitespace-nowrap uppercase text-white"
+              className="text-sm font-bold leading-relaxed inline-block mr-4  whitespace-nowrap uppercase text-yellow-100"
               href="#pablo"
             >
-             <img src={wiccanCollective} alt=""  className='w-[40%] 3xl:w-[18%]'/>
+             <img src={wiccanCollective} alt=""  className='w-[25%] 3xl:w-[25%]'/>
             </a>
             </NavLink>
            
@@ -35,42 +35,94 @@ const NavbarComponent = ({fixed}) => {
           </div>
           <div
             className={
-              "lg:flex flex-grow items-center" +
+              "lg:flex flex-grow items-center absolute top-[101%] right-[.01rem] z-[100]  w-[80%]  lg:top-[40%]  " +
               (navbarOpen ? " flex" : " hidden")
             }
             id="example-navbar-danger"
-          >
+          >      
+      
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
               <NavLink to="/products">
               <li className="nav-item ">
                 <a
-                  className="px-3  flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  className="px-3  flex items-center text-xs uppercase font-bold leading-snug text-yellow-100 "
                   href="#pablo"
                 >
-                  <i className="text-lg leading-lg text-white opacity-75"></i><span className="ml-2 text-[1.7rem] font-Harrington">Products</span>
+                  <i className="text-lg leading-lg text-yellow-100 opacity-75"></i><span className="ml-2 text-[1.2rem] font-Harrington">Products</span>
                 </a>
               </li>
               </NavLink>
              
               <li className="nav-item">
                 <a
-                  className="px-3  flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  className="px-3  flex items-center text-xs uppercase font-bold leading-snug text-yellow-100 "
                   href="#pablo"
                 >
-                  <i className=" text-lg leading-lg text-white opacity-75"></i><span className="ml-2 text-[1.7rem] font-Harrington">Tweet</span>
+                  <i className=" text-lg leading-lg text-yellow-100 opacity-75"></i><span className="ml-2 text-[1.2rem] font-Harrington">About</span>
                 </a>
               </li>
               <NavLink to="/cart">
               <li className="nav-item">
                 <a
-                  className="px-3  flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  className="px-3  flex items-center text-xs uppercase font-bold leading-snug text-yellow-100 "
                   href="#pablo"
                 >
-                  <i className=" text-lg leading-lg text-white opacity-75"></i><span className="ml-2 text-[1.7rem] font-Harrington">Bag {cartProducts.length} </span>
+                  <i className=" text-lg leading-lg text-yellow-100 "></i><span className="ml-2 text-[1.2rem] font-Harrington">Bag {cartProducts.length} </span>
                 </a>
               </li>
               </NavLink>
-              
+              <NavLink to="/policies">
+              <li className="nav-item ">
+                <a
+                  className="px-3  flex items-center text-xs uppercase font-bold leading-snug text-yellow-100 "
+                  href="#pablo"
+                >
+                  <i className="text-lg leading-lg text-yellow-100 "></i><span className="ml-2 text-[1.2rem] font-Harrington">Policies</span>
+                </a>
+              </li>
+              </NavLink>
+
+              <NavLink to="/blog">
+              <li className="nav-item ">
+                <a
+                  className="px-3  flex items-center text-xs uppercase font-bold leading-snug text-yellow-100 "
+                  href="#pablo"
+                >
+                  <i className="text-lg leading-lg text-yellow-100 "></i><span className="ml-2 text-[1.2rem] font-Harrington">Blog</span>
+                </a>
+              </li>
+              </NavLink>
+
+              <NavLink to="/products">
+              <li className="nav-item ">
+                <a
+                  className="px-3  flex items-center text-xs uppercase font-bold leading-snug text-yellow-100 "
+                  href="#pablo"
+                >
+                  <i className="text-lg leading-lg text-yellow-100 opacity-75"></i><span className="ml-2 text-[1.2rem] font-Harrington">Products</span>
+                </a>
+              </li>
+              </NavLink>
+              <NavLink to="/faqs">
+              <li className="nav-item ">
+                <a
+                  className="px-3  flex items-center text-xs uppercase font-bold leading-snug text-yellow-100 "
+                  href="#pablo"
+                >
+                  <i className="text-lg leading-lg text-yellow-100 opacity-75"></i><span className="ml-2 text-[1.2rem] font-Harrington">Faqs</span>
+                </a>
+              </li>
+              </NavLink>
+              <NavLink to="/contact">
+              <li className="nav-item ">
+                <a
+                  className="px-3  flex items-center text-xs uppercase font-bold leading-snug text-yellow-100 "
+                  href="#pablo"
+                >
+                  <i className="text-lg leading-lg text-yellow-100 opacity-75"></i><span className="ml-2 text-[1.2rem] font-Harrington">Contact</span>
+                </a>
+              </li>
+              </NavLink>
             </ul>
           </div>
         </div>
