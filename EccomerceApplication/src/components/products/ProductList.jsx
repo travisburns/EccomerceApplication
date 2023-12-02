20d1153fe0e628d45b6f6f7e75c508015813a86b
Travis <travisjb0@gmail.com>
2023-12-01 21:03:04 -0800

    added products design in a better way

--- EccomerceApplication/src/components/products/ProductList.jsx
+++ EccomerceApplication/src/components/products/ProductList.jsx
@@ -24,24 +24,27 @@ const ProductList = ({products, status}) => {
         <section className='product py-5 bg-ghost-white' id = "products">
             { isModalVisible && <SingleProduct />}
 
-            <div className='px-8'>
+            <div className='px-8 xl:px-28'>
                 <div className='product-content'>
                     <div className='section-title'>
-                        <h3 className='text-uppercase text-[3rem] 2xl:text-[9rem] text-yellow-100 font-Harrington'>Our Products</h3>
+                        <h3 className='text-uppercase text-[3rem] xl:text-[6rem] 2xl:text-[9rem] text-yellow-100 font-Harrington'>Our Products</h3>
                     </div>
                     <div className='product-items flex flex-wrap'>
                         {
                             products.slice(0, 20).map(product => (
-                                <div className='product-item bg-white w-[41%]  mx-3 my-3' key = {product.id} onClick = {() => viewModalHandler(product)}>
-                                    <div className='product-item-img'>
-                                        <img src = {product.images[0]} alt = "" className='max-h-80 '/>
-                                        <div className = "product-item-cat text-white fs-13 text-uppercase bg-gold fw-6">{product.category.name}</div>
-                                    </div>
+                                <div className='product-item bg-violet-900 w-[46.8%] xl:w-[48.8%]  mx-1 my-3' key = {product.id} onClick = {() => viewModalHandler(product)}>
+                                    <div className='product-item-img w-[100%]'>
+                                        <img src = {product.images[0]} alt = "" className='w-[100%] min-h-[10rem]'/>
+                                      
+                                   
                                     <div className='product-item-body bg-violet-900 text-white font-Harrington text-[1.4rem]'>
-                                        <h6 className = "product-item-title text-pine-green fw-4 fs-15">{product.title}</h6>
-                                        <div className = "product-item-price text-regal-blue fw-7 fs-18">{formatPrice(product.price)}</div>
+                                    <h6 className = "product-item-title text-pine-green bg-yellow-100 text-black fw-4 fs-15">{product.title}</h6>
+                                    <div className = "product-item-cat text-black font-bold  font-Harrington text-[1.6rem] fs-13 text-uppercase bg-gold fw-6">{product.category.name}</div>
+                                        
+                                        <div className = "product-item-price text-green-500 fw-7 fs-18">{formatPrice(product.price)}</div>
                                         
                                     </div>
+                                </div>
                                 </div>
                             ))
                         }
