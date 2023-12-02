eb5569b0da0eb818351acfd7feb0d414e549e690
Travis <travisjb0@gmail.com>
2023-12-01 20:38:22 -0800

    Added more edits to categories

--- EccomerceApplication/src/components/categorypage/CategoryPage.jsx
+++ EccomerceApplication/src/components/categorypage/CategoryPage.jsx
@@ -13,21 +13,21 @@ const Category = ({ categories, status, }) => {
     console.log(categories);
 
   return (
-    <section className='categories  text-center px-8' id = 'categories'>
+    <section className='categories  px-8 ' id = 'categories'>
        
         <div className='section-title'>
             
                 
-                    <h3 className='text-yellow-100 font-Harrington text-[3rem] md:text-[4rem] lg:text-[6rem] 2xl:text-[12rem] 3xl:text-[24rem] pb-10'>Categories</h3>
+                    <h3 className='text-yellow-100 font-Harrington text-[3rem] sm:text-[3.5rem] md:text-[4rem] lg:text-[6rem] 2xl:text-[12rem] 3xl:text-[13rem] pb-10'>Categories</h3>
                 
                 </div>
-                <div className='category-items flex flex-wrap justify-between md:justify-center w-[100%]'>
+                <div className='category-items flex flex-wrap justify-between md:justify-center w-[100%] 2xl:ml-[2.5rem]'>
                     {
                         categories.slice(0, 5).map(category =>  (
-                            <Link to={`category/${category.id}`} key = {categories.id} className='w-[46%] md:w-[47%] lg:w-[29%]  lg:mr-[1rem] mb-6'>
+                            <Link to={`category/${category.id}`} key = {categories.id} className='w-[46%] sm:mb-[2.5rem] md:w-[47%] md:mb-[.8rem] lg:w-[29%]  lg:mr-[1rem] mb-6 2xl:mb-[5rem]'>
                                 <div className='category-item '>
-                                    <div className='category-item-img '>
-                                        <img src = {category.image} alt= "" className='max-h-80 ' />
+                                    <div className='category-item-img 3xl:w-[100%]'>
+                                        <img src = {category.image} alt= "" className='max-h-80 2xl:w-[75%]' />
                                     </div>
                                     <div className='category-item-name text-center '>
                                         <h6 className='text-yellow-100 font-Harrington text-[1.5rem] 2xl:text-[3rem] bg-violet-700 w-[100%] md:w-[96.6%] lg:w-[100%] xl:w-[90.9%] 2xl:w-[75%] '>{category.name}</h6>
